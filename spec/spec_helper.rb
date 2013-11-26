21d85564b06128231245e881297323df454819df
Maximilian Seifert <maximilian.cypher@gmail.com>
2013-11-26 16:36:05 +0100

    Add default adapter options

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,8 +7,6 @@ require 'sequel'
 require 'logger'
 
 options = {
-  client_min_messages: false,
-  force_standard_strings: false,
   logger: Logger.new(STDOUT)
 }
 DB = Sequel.connect('redshift://remind101:wvpfSzw2TT8COE@r101-dw-production.ck1sr65ao7pg.us-east-1.redshift.amazonaws.com:5439/test1', options)
