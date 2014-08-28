a6fc7171f85192c71217d7d3cabf979da56fcf7e
Roland Swingler <roland.swingler@gmail.com>
2014-08-28 16:46:55 +0100

    Remove hardcoded database connection from spec_helper.
    This is instead loaded from spec/database.yml - an example
    file is provided. This allows different people to run tests
    on their own redshift clusters.
    
    In addition, tests have been moved under a test-specific
    schema - this needs to be created before runnning tests on
    your cluster.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,8 +5,23 @@ Bundler.require :default
 
 require 'sequel'
 require 'logger'
+require 'yaml'
 
-options = {
-  logger: Logger.new(STDOUT)
-}
-DB = Sequel.connect('redshift://remind101:wvpfSzw2TT8COE@r101-dw-production.ck1sr65ao7pg.us-east-1.redshift.amazonaws.com:5439/test1', options)
+$: << "."
+require 'sequel/redshift'
+
+config_path = File.join(File.dirname(__FILE__), "database.yml")
+
+unless File.exists?(config_path)
+  warn "spec/database.yml does not exist."
+  warn "Create it based on spec/database.yml.example\nto conenct to a redshift cluster."
+  exit 1
+end
+
+options = YAML.load(File.read(config_path))
+options.merge(logger: Logger.new(STDOUT))
+
+DB = Sequel.connect(options)
+# Run all the tests in a specific test schema
+DB.run "set search_path to 'sequel_redshift_adapter_test'"
+
